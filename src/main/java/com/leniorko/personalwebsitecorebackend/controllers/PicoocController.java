829ab117282da8bf717387ca9fd25c599af98ee2
Ivan Egorov <www.Len244@gmail.com>
2022-07-03 23:06:24 +0300

    Basic file upload handling

--- src/main/java/com/leniorko/personalwebsitecorebackend/controllers/PicoocController.java
+++ src/main/java/com/leniorko/personalwebsitecorebackend/controllers/PicoocController.java
@@ -1,8 +1,16 @@
 package com.leniorko.personalwebsitecorebackend.controllers;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class PicoocController {
@@ -22,12 +30,26 @@ public class PicoocController {
   }
 
   @PostMapping("/upload-picooc-tsv")
-  public static String uploadPicoocTsv() {
-    // TODO: Write upload logic
-    // TODO: Write parsing logic
-    // TODO: Write database save logic
+  public static ResponseEntity<?> uploadPicoocTsv(@RequestParam("picooc_file") MultipartFile uploadedFile) {
     // TODO: Write validation and proper error handling
     // TODO: Write login some auth logic to prevent unauthicated acces to endpoint
-    return "Will return every row from picooc_data_average view";
+
+    try {
+      InputStream uploadedFileInputStream = uploadedFile.getInputStream();
+      ;
+      BufferedReader reader = new BufferedReader(new InputStreamReader(uploadedFileInputStream));
+      while (reader.ready()) {
+        String currentLine = reader.readLine();
+        System.out.println(currentLine);
+        // TODO: Write parsing logic
+        // TODO: Write database save logic
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      return ResponseEntity.internalServerError().body("Can't read file input stream");
+    }
+
+    // return "Will return every row from picooc_data_average view";\
+    return ResponseEntity.ok("File uploaded successfully");
   }
 }
